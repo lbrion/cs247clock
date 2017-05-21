eb6fac8924bafa9759f644fcef3c0d356cffaecb
STZhang <zhangst@cs.stanford.edu>
2017-05-21 12:20:42 -0700

    change to 3 clocks

---
+++ input.js
@@ -0,0 +1,108 @@
+
+  input = [[{
+    "region": "East",
+    "fruit": "Zero",
+    "count": "0"
+  }, {
+    "region": "West",
+    "fruit": "Zero",
+    "count": "100"
+  }, {
+    "region": "East",
+    "fruit": "One",
+    "count": "33"
+  }, {
+    "region": "West",
+    "fruit": "One",
+    "count": "66"
+  }, {
+    "region": "East",
+    "fruit": "Two",
+    "count": "66"
+  }, {
+    "region": "West",
+    "fruit": "Two",
+    "count": "33"
+  }, {
+    "region": "East",
+    "fruit": "Three",
+    "count": "100"
+  }, {
+    "region": "West",
+    "fruit": "Three",
+    "count": "0"
+  }], 
+[{
+    "region": "East",
+    "fruit": "Zero",
+    "count": "0"
+  }, {
+    "region": "West",
+    "fruit": "Zero",
+    "count": "100"
+  }, {
+    "region": "East",
+    "fruit": "One",
+    "count": "33"
+  }, {
+    "region": "West",
+    "fruit": "One",
+    "count": "66"
+  }, {
+    "region": "East",
+    "fruit": "Two",
+    "count": "66"
+  }, {
+    "region": "West",
+    "fruit": "Two",
+    "count": "33"
+  }, {
+    "region": "East",
+    "fruit": "Three",
+    "count": "100"
+  }, {
+    "region": "West",
+    "fruit": "Three",
+    "count": "0"
+  }],
+[{
+    "region": "East",
+    "fruit": "Zero",
+    "count": "0"
+  }, {
+    "region": "West",
+    "fruit": "Zero",
+    "count": "100"
+  }, {
+    "region": "East",
+    "fruit": "One",
+    "count": "33"
+  }, {
+    "region": "West",
+    "fruit": "One",
+    "count": "66"
+  }, {
+    "region": "East",
+    "fruit": "Two",
+    "count": "66"
+  }, {
+    "region": "West",
+    "fruit": "Two",
+    "count": "33"
+  }, {
+    "region": "East",
+    "fruit": "Three",
+    "count": "100"
+  }, {
+    "region": "West",
+    "fruit": "Three",
+    "count": "0"
+  }]
+  ]
+
+var data = [
+  [11975,  5871, 8916, 2868],
+  [ 1951, 10048, 2060, 6171],
+  [ 8010, 16145, 8090, 8045],
+  [ 1013,   990,  940, 6907]
+];
