25bb5550d178810200406322b4cbb586cc12805d
STZhang <zhangst@cs.stanford.edu>
2017-06-03 00:35:54 -0700

    progress page

---
+++ progress.js
@@ -0,0 +1,59 @@
+function firstRow() {
+	document.getElementById("col2.1").style.background = "linear-gradient(to bottom, #265649 , #95efcb)";
+	document.getElementById("col2.2").style.background = "linear-gradient(to bottom, #9ab3ac , #d0f8e8)";
+	document.getElementById("col2.3").style.background = "linear-gradient(to bottom, #9ab3ac , #d0f8e8)";
+	document.getElementById("col2.4").style.background = "linear-gradient(to bottom, #9ab3ac , #d0f8e8)";
+	document.getElementById("col2.1").style.overflowY = "scroll";
+	document.getElementById("col2.2").style.overflowY = "hidden";
+	document.getElementById("col2.3").style.overflowY = "hidden";
+	document.getElementById("col2.4").style.overflowY = "hidden";
+	document.getElementById("col1.1").style.color = "#525153";
+	document.getElementById("col1.2").style.color = "#ffffff";
+	document.getElementById("col1.3").style.color = "#ffffff";
+	document.getElementById("col1.4").style.color = "#ffffff";
+}
+
+function secondRow() {
+	document.getElementById("col2.1").style.background = "linear-gradient(to bottom, #9ab3ac , #d0f8e8)";
+	document.getElementById("col2.2").style.background = "linear-gradient(to bottom, #265649 , #95efcb)";
+	document.getElementById("col2.3").style.background = "linear-gradient(to bottom, #9ab3ac , #d0f8e8)";
+	document.getElementById("col2.4").style.background = "linear-gradient(to bottom, #9ab3ac , #d0f8e8)";
+	document.getElementById("col2.2").style.overflowY = "scroll";
+	document.getElementById("col2.1").style.overflowY = "hidden";
+	document.getElementById("col2.3").style.overflowY = "hidden";
+	document.getElementById("col2.4").style.overflowY = "hidden";
+	document.getElementById("col1.2").style.color = "#525153";
+	document.getElementById("col1.1").style.color = "#ffffff";
+	document.getElementById("col1.3").style.color = "#ffffff";
+	document.getElementById("col1.4").style.color = "#ffffff";
+}
+
+function thirdRow() {
+	document.getElementById("col2.1").style.background = "linear-gradient(to bottom, #9ab3ac , #d0f8e8)";
+	document.getElementById("col2.2").style.background = "linear-gradient(to bottom, #9ab3ac , #d0f8e8)";
+	document.getElementById("col2.3").style.background = "linear-gradient(to bottom, #265649 , #95efcb)";
+	document.getElementById("col2.4").style.background = "linear-gradient(to bottom, #9ab3ac , #d0f8e8)";
+	document.getElementById("col2.3").style.overflowY = "scroll";
+	document.getElementById("col2.1").style.overflowY = "hidden";
+	document.getElementById("col2.2").style.overflowY = "hidden";
+	document.getElementById("col2.4").style.overflowY = "hidden";
+	document.getElementById("col1.3").style.color = "#525153";
+	document.getElementById("col1.2").style.color = "#ffffff";
+	document.getElementById("col1.1").style.color = "#ffffff";
+	document.getElementById("col1.4").style.color = "#ffffff";
+}
+
+function fourthRow() {
+	document.getElementById("col2.1").style.background = "linear-gradient(to bottom, #9ab3ac , #d0f8e8)";
+	document.getElementById("col2.2").style.background = "linear-gradient(to bottom, #9ab3ac , #d0f8e8)";
+	document.getElementById("col2.3").style.background = "linear-gradient(to bottom, #9ab3ac , #d0f8e8)";
+	document.getElementById("col2.4").style.background = "linear-gradient(to bottom, #265649 , #95efcb)";
+	document.getElementById("col2.4").style.overflowY = "scroll";
+	document.getElementById("col2.1").style.overflowY = "hidden";
+	document.getElementById("col2.2").style.overflowY = "hidden";
+	document.getElementById("col2.3").style.overflowY = "hidden";
+	document.getElementById("col1.4").style.color = "#525153";
+	document.getElementById("col1.2").style.color = "#ffffff";
+	document.getElementById("col1.3").style.color = "#ffffff";
+	document.getElementById("col1.1").style.color = "#ffffff";
+}
